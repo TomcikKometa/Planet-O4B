63708254efbe951b349d2cb432a9aa7468207a61
TomcikKometa <tomcik48_84@o2.pl>
2023-08-08 10:45:52 +0200

    Add manage misions module

---
+++ src/app/modules/manage-missions/manage-missions.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ManageMissionsRoutingModule } from './manage-missions-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ManageMissionsRoutingModule
+  ]
+})
+export class ManageMissionsModule { }
