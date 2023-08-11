bc11a3f8c27d162ce412a1a180ddf29a4e4b37dc
TomcikKometa <tomcik48_84@o2.pl>
2023-08-11 08:40:35 +0200

    Add mat-dialog add-mision

--- src/app/modules/manage-missions/components/add-missision-dialog/add-missision-dialog.component.ts
+++ src/app/modules/manage-missions/components/add-missision-dialog/add-missision-dialog.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { DateAdapter } from '@angular/material/core';
+
 
 @Component({
   selector: 'planet-add-missision-dialog',
   templateUrl: './add-missision-dialog.component.html',
-  styleUrls: ['./add-missision-dialog.component.scss']
+  styleUrls: ['./add-missision-dialog.component.scss'],
 })
 export class AddMissisionDialogComponent {
 
+  constructor(private dateAdapter: DateAdapter<Date>) {
+    this.dateAdapter.setLocale('pl');   
+  }
 
 }
